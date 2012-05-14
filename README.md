a8f4f2f94116718a957dc3f1568c8969bb722463
Roland Moriz <roland@moriz.de>
2012-05-14 18:54:08 +0200

    Initial import.

---
+++ README.md
@@ -0,0 +1,28 @@
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* capybara
+* cucumber
+* rails_basics
+* git
+* github
+* guard
+* jquery
+* rspec
+* twitter_bootstrap
+* simple_form
+* sqlite3
+* unicorn
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piraten6::Application.config.secret_token = 'be0acea61ce42206d274ca07ea4bb519b2a3c25d8dfd8c6c1884fcbf244ba755f561cabe24d14de6e01ab407e6b4ebab4d3f5fc3c7d22efc1abb62a5cc779e3f'
