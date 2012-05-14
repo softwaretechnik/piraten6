3f06d009b6523abc7cb655128cbc0deb0eb0e301
Roland Moriz <roland@moriz.de>
2012-05-14 23:15:09 +0200

    state names

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,2 +1,40 @@
+# encoding: UTF-8
 module ServicesHelper
+
+  def state_to_human(state)
+      case state
+      when 'BW'
+        'Baden-Württemberg'
+      when 'BY'
+        'Bayern'
+      when 'BE'
+        'Berlin'
+      when 'BB'
+        'Brandenburg'
+      when 'HB'
+        'Bremen'
+      when 'HH'
+        'Hamburg'
+      when 'HE'
+        'Hessen'
+      when 'MV'
+        'Mecklenburg-Vorpommern'
+      when 'NI'
+        'Niedersachsen'
+      when 'NW'
+        'Nordrhein-Westfalen'
+      when 'RP'
+        'Rheinland-Pfalz'
+      when 'SL'
+        'Saarland'
+      when 'SN'
+        'Sachsen'
+      when 'ST'
+        'Sachsen-Anhalt'
+      when 'SH'
+        'Schleswig-Holstein'
+      when 'TH'
+        'Thüringen'
+      end
+  end
 end
