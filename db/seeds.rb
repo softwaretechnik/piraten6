4fff7daa4f7200289faddcdea8ba0e557dbce126
Roland Moriz <roland@moriz.de>
2012-05-15 20:55:42 +0200

    moar seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,29 +10,30 @@
 
 Service.delete_all
 
-Service.create hostname: 'www.piratenpartei.de',               description: 'Website der Piratenpartei Deutschland',    country: 'DE',           show: true
-Service.create hostname: 'wiki.piratenpartei.de',              description: 'Wiki Piratenpartei Deutschland',           country: 'DE',           show: true
-Service.create hostname: 'lqfb.piratenpartei.de',              description: 'LiquidFeedback Piratenpartei Deutschland', country: 'DE',           show: true
-Service.create hostname: 'www.piratenpad.de',                  description: 'Freier Etherpad Server',                   country: 'DE',           show: true
-Service.create hostname: 'www.piratenpartei-bayern.de',        description: 'Website des LV Bayern',                 country: 'DE', state: 'BY', show: true
-Service.create hostname: 'www.piratenpartei-nrw.de',           description: 'Website des LV Nordrhein-Westfalen',    country: 'DE', state: 'NW', show: true
-Service.create hostname: 'www.piratenpartei-sh.de',            description: 'Website des LV Schleswig-Holstein',     country: 'DE', state: 'SH', show: true
-Service.create hostname: 'www.piratenpartei-bw.de',            description: 'Website des LV Baden-Württemberg',      country: 'DE', state: 'BW', show: true
-Service.create hostname: 'www.piratenpartei-hessen.de',        description: 'Website des LV Hessen',                 country: 'DE', state: 'HE', show: true
-Service.create hostname: 'www.piraten-rlp.de',                 description: 'Website des LV Rheinland-Pfalz',        country: 'DE', state: 'RP', show: true
-Service.create hostname: 'www.piratenpartei-saarland.de',      description: 'Website des LV Saarland',               country: 'DE', state: 'SL', show: true
-Service.create hostname: 'bremen.piratenpartei.de',            description: 'Website des LV Bremen',                 country: 'DE', state: 'HB', show: true
-Service.create hostname: 'www.piratenpartei-hamburg.de',       description: 'Website des LV Hamburg',                country: 'DE', state: 'HH', show: true
-Service.create hostname: 'www.piratenpartei-niedersachsen.de', description: 'Website des LV Niedersachsen',          country: 'DE', state: 'NI', show: true
-Service.create hostname: 'berlin.piratenpartei.de',            description: 'Website des LV Berlin',                 country: 'DE', state: 'BE', show: true
-Service.create hostname: 'www.piratenbrandenburg.de',          description: 'Website des LV Brandenburg',            country: 'DE', state: 'BB', show: true
-Service.create hostname: 'www.piraten-sachsen.de',             description: 'Website des LV Sachsen',                country: 'DE', state: 'SN', show: true
-Service.create hostname: 'www.piraten-lsa.de',                 description: 'Website des LV Sachsen-Anhalt',         country: 'DE', state: 'ST', show: true
-Service.create hostname: 'piratenpartei-mv.de',                description: 'Website des LV Mecklenburg-Vorpommern', country: 'DE', state: 'MV', show: true
-Service.create hostname: 'www.piraten-thueringen.de',          description: 'Website des LV Thüringen',              country: 'DE', state: 'TH', show: true
+Service.create hostname: 'www.piratenpartei.de',               description: 'Website der Piratenpartei Deutschland',    country: 'DE',              show: true
+Service.create hostname: 'wiki.piratenpartei.de',              description: 'Wiki Piratenpartei Deutschland',           country: 'DE',              show: true
+Service.create hostname: 'lqfb.piratenpartei.de',              description: 'LiquidFeedback Piratenpartei Deutschland', country: 'DE',              show: true
+Service.create hostname: 'www.piratenpad.de',                  description: 'Freier Etherpad Server',                   country: 'DE',              show: true
+Service.create hostname: 'www.junge-piraten.de',               description: 'JuPis',                                    country: 'DE',              show: true
+Service.create hostname: 'www.piratenpartei-bayern.de',        description: 'Website des LV Bayern',                    country: 'DE', state: 'BY', show: true
+Service.create hostname: 'www.piratenpartei-nrw.de',           description: 'Website des LV Nordrhein-Westfalen',       country: 'DE', state: 'NW', show: true
+Service.create hostname: 'www.piratenpartei-sh.de',            description: 'Website des LV Schleswig-Holstein',        country: 'DE', state: 'SH', show: true
+Service.create hostname: 'www.piratenpartei-bw.de',            description: 'Website des LV Baden-Württemberg',         country: 'DE', state: 'BW', show: true
+Service.create hostname: 'www.piratenpartei-hessen.de',        description: 'Website des LV Hessen',                    country: 'DE', state: 'HE', show: true
+Service.create hostname: 'www.piraten-rlp.de',                 description: 'Website des LV Rheinland-Pfalz',           country: 'DE', state: 'RP', show: true
+Service.create hostname: 'www.piratenpartei-saarland.de',      description: 'Website des LV Saarland',                  country: 'DE', state: 'SL', show: true
+Service.create hostname: 'bremen.piratenpartei.de',            description: 'Website des LV Bremen',                    country: 'DE', state: 'HB', show: true
+Service.create hostname: 'www.piratenpartei-hamburg.de',       description: 'Website des LV Hamburg',                   country: 'DE', state: 'HH', show: true
+Service.create hostname: 'www.piratenpartei-niedersachsen.de', description: 'Website des LV Niedersachsen',             country: 'DE', state: 'NI', show: true
+Service.create hostname: 'berlin.piratenpartei.de',            description: 'Website des LV Berlin',                    country: 'DE', state: 'BE', show: true
+Service.create hostname: 'www.piratenbrandenburg.de',          description: 'Website des LV Brandenburg',               country: 'DE', state: 'BB', show: true
+Service.create hostname: 'www.piraten-sachsen.de',             description: 'Website des LV Sachsen',                   country: 'DE', state: 'SN', show: true
+Service.create hostname: 'www.piraten-lsa.de',                 description: 'Website des LV Sachsen-Anhalt',            country: 'DE', state: 'ST', show: true
+Service.create hostname: 'piratenpartei-mv.de',                description: 'Website des LV Mecklenburg-Vorpommern',    country: 'DE', state: 'MV', show: true
+Service.create hostname: 'www.piraten-thueringen.de',          description: 'Website des LV Thüringen',                 country: 'DE', state: 'TH', show: true
 
 
-# BaWü BzV
+# BaWü BzV BW
 #
 %w(
   piraten-bzv-stuttgart.de
@@ -43,7 +44,7 @@ Service.create hostname: 'www.piraten-thueringen.de',          description: 'Web
   Service.create hostname: host, description: "-", country: 'DE', state: 'BW', show: true
 end
 
-# BaWü KV
+# BaWü KVs BW
 #
 %w(
   piraten-rvfn.de
@@ -61,10 +62,7 @@ end
   Service.create hostname: host, description: "-", country: 'DE', state: 'BW', show: true
 end
 
-
-# Berliner
-#
-#
+# Berlin BE
 %w(
   www.piratenpartei-charlottenburg-wilmersdorf.de Charlottenburg-Wilmersdorf
   piraten-friedrichshain-kreuzberg.de Friedrichshain-Kreuzberg
@@ -83,7 +81,7 @@ end
   Service.create hostname: host, description: descr, country: 'DE', state: 'BE', show: true
 end
 
-# Brandenburg
+# Brandenburg BB
 %w(
   www.piraten-potsdam.de
   brb.piratenbrandenburg.de
@@ -98,7 +96,7 @@ end
   Service.create hostname: host, description: "-", country: 'DE', state: 'BB', show: true
 end
 
-# Bayern BZ
+# Bayern BY
 %w(
   piraten-mfr.de
   piraten-ufr.de
@@ -266,6 +264,71 @@ end
 ).each do |host|
   Service.create hostname: host, description: "-", country: 'DE', state: 'NW', show: true
 end
+
+# RLP KV
+%w(
+  piratenpartei-koblenz.de
+  www.piraten-mittelhaardt.de
+  piraten-rp.de
+  piraten-trier.de
+).each do |host|
+  Service.create hostname: host, description: "-", country: 'DE', state: 'RP', show: true
+end
+
+# Saarland SL
+%w(
+
+).each do |host|
+  Service.create hostname: host, description: "-", country: 'DE', state: 'SL', show: true
+end
+
+
+# Sachsen SN
+%w(
+  piraten-leipzig.de
+  piraten-chemnitz.de
+  piraten-goerlitz.de
+  piraten-dresden.de
+  piraten-erzgebirge.de
+  piraten-soe.de
+  piraten-bautzen.de
+  piratenpartei-vogtland.de
+).each do |host|
+  Service.create hostname: host, description: "-", country: 'DE', state: 'SN', show: true
+end
+
+# Sachsen-Anhalt ST
+%w(
+  www.piraten-altmark.de
+  piraten-anhalt.de
+  www.piraten-magdeburg.de
+  hsg.piraten-halle.de
+  hsg-md.piraten-lsa.de
+  salzwedel.piraten-altmark.de
+  stendal.piraten-altmark.de
+
+).each do |host|
+  Service.create hostname: host, description: "-", country: 'DE', state: 'ST', show: true
+end
+
+# Thüringen TN
+%w(
+  piraten-altenburger-land.de
+  piraten-gotha.de
+  wartburgpiraten.de
+
+).each do |host|
+  Service.create hostname: host, description: "-", country: 'DE', state: 'TH', show: true
+end
+
+# Schleswig-Holstein SH
+%w(
+  piratenpartei-flensburg.de
+  piratenpartei-luebeck.de
+).each do |host|
+  Service.create hostname: host, description: "-", country: 'DE', state: 'SH', show: true
+end
+
 
 Service.update_addresses!
 
